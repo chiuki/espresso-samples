c1f1978c300629129430d1fd6cc0bfd1cae60860
Chiu-Ki Chan <chiuki@gmail.com>
2015-05-08 12:31:28 -0600

    Match Toolbar title using getTitle() in custom matcher

--- toolbar-title/README.md
+++ toolbar-title/README.md
@@ -1,6 +1,6 @@
 Match Toolbar Title
 -------------------
 
-Match the `Toolbar` title by looking for a TextView with Toolbar as parent.
+Match the `Toolbar` title using `getTitle()` in custom matcher.
 
 Blog post: http://blog.sqisland.com/2015/05/espresso-match-toolbar-title.html
--- toolbar-title/app/src/androidTest/java/com/sqisland/espresso/toolbar_title/MainActivityTest.java
+++ toolbar-title/app/src/androidTest/java/com/sqisland/espresso/toolbar_title/MainActivityTest.java
@@ -2,11 +2,13 @@ package com.sqisland.espresso.toolbar_title;
 
 import android.support.test.InstrumentationRegistry;
 import android.support.test.espresso.ViewInteraction;
+import android.support.test.espresso.matcher.BoundedMatcher;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 import android.support.v7.widget.Toolbar;
-import android.widget.TextView;
 
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -14,9 +16,7 @@ import org.junit.runner.RunWith;
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.isAssignableFrom;
-import static android.support.test.espresso.matcher.ViewMatchers.withParent;
-import static android.support.test.espresso.matcher.ViewMatchers.withText;
-import static org.hamcrest.Matchers.allOf;
+import static org.hamcrest.core.Is.is;
 
 @RunWith(AndroidJUnit4.class)
 public class MainActivityTest {
@@ -30,10 +30,19 @@ public class MainActivityTest {
   }
 
   private static ViewInteraction matchToolbarTitle(CharSequence title) {
-    return onView(
-        allOf(
-            withParent(isAssignableFrom(Toolbar.class)),
-            isAssignableFrom(TextView.class)))
-        .check(matches(withText(title.toString())));
+    return onView(isAssignableFrom(Toolbar.class))
+        .check(matches(withToolbarTitle(is(title))));
+  }
+
+  private static Matcher<Object> withToolbarTitle(final Matcher<CharSequence> textMatcher) {
+    return new BoundedMatcher<Object, Toolbar>(Toolbar.class) {
+      @Override public boolean matchesSafely(Toolbar toolbar) {
+        return textMatcher.matches(toolbar.getTitle());
+      }
+      @Override public void describeTo(Description description) {
+        description.appendText("with toolbar title: ");
+        textMatcher.describeTo(description);
+      }
+    };
   }
 }
