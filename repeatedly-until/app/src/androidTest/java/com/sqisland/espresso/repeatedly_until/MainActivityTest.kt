4db52be85f95bad6f79762422abb91c93fc02d45
Chiu-Ki Chan <chiuki@users.noreply.github.com>
2019-01-16 18:41:29 -0700

    Upgrade repeatedly-until to AndroidX

--- repeatedly-until/app/src/androidTest/java/com/sqisland/espresso/repeatedly_until/MainActivityTest.kt
+++ repeatedly-until/app/src/androidTest/java/com/sqisland/espresso/repeatedly_until/MainActivityTest.kt
@@ -1,17 +1,18 @@
 package com.sqisland.espresso.repeatedly_until
 
-import android.support.test.rule.ActivityTestRule
-
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.action.ViewActions.repeatedlyUntil
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.matcher.ViewMatchers.withText
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.rule.ActivityTestRule
 import org.junit.Rule
 import org.junit.Test
+import org.junit.runner.RunWith
 
-import android.support.test.espresso.Espresso.onView
-import android.support.test.espresso.action.ViewActions.click
-import android.support.test.espresso.assertion.ViewAssertions.matches
-import android.support.test.espresso.matcher.ViewMatchers.withId
-import android.support.test.espresso.matcher.ViewMatchers.withText
-import android.support.test.espresso.action.ViewActions.repeatedlyUntil
-
+@RunWith(AndroidJUnit4::class)
 class MainActivityTest {
   @get:Rule
   val activityRule: ActivityTestRule<MainActivity> = ActivityTestRule(MainActivity::class.java)
